aa6b4cd5ad7104352033f9f7a824b0eeb66e6031
chalalala <neko_hime1212@yahoo.com>
2020-10-31 23:29:15 +0700

    Add pie chart

--- Dashboard.js
+++ Dashboard.js
@@ -1,19 +1,64 @@
 import React from 'react';
 import Constants from 'expo-constants';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LinearGradient from 'react-native-linear-gradient';
-import { ScrollView } from 'react-native-gesture-handler';
+// import LinearGradient from 'react-native-linear-gradient';
+import { Dimensions } from "react-native";
+import { data } from './data.js';
+
+import {
+   LineChart,
+   BarChart,
+   PieChart,
+   ProgressChart,
+   ContributionGraph,
+   StackedBarChart
+ } from "react-native-chart-kit";
 
 const Stack = createStackNavigator();
+const screenWidth = Dimensions.get("window").width;
+
+const chartConfig = {
+   backgroundGradientFrom: "#1E2923",
+   backgroundGradientFromOpacity: 0,
+   backgroundGradientTo: "#08130D",
+   backgroundGradientToOpacity: 0.5,
+   color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+   strokeWidth: 2, // optional, default 3
+   barPercentage: 0.5,
+   useShadowColorFromDataset: false // optional
+ };
 
 const DashboardScreen = ({navigator}) => {
    return(
-      <View style={styles.container}>
-         <ScrollView>
-            <Text>Hello</Text>
-         </ScrollView>
-      </View>
+      <ScrollView>
+         <View style={styles.container}>
+            <View style={styles.card}>
+               <View>
+                  <Text style={styles.label}>Active user</Text>
+                  <Text>300/400</Text>
+               </View>
+            </View>
+            
+            <View style={styles.card}>
+               <Text style={styles.label}>Total call</Text>
+               <Text>10.000.000</Text>
+            </View>
+
+            <View style={styles.chartContainer}>
+               <Text style={styles.label}>Distribution of users</Text>
+               <PieChart
+                  data={data}
+                  width={screenWidth*0.8}
+                  height={170}
+                  chartConfig={chartConfig}
+                  accessor="percentage"
+                  backgroundColor="transparent"
+                  paddingLeft="0"
+               />
+            </View>
+         </View>
+      </ScrollView>
    )
 }
 
@@ -43,9 +88,50 @@ export default function Dashboard({route}){
 const styles = StyleSheet.create({
    container: {
      flex: 1,
-     backgroundColor: '#fff',
      alignItems: 'center',
-     justifyContent: 'center',
    },
+   content: {
+      width:'100%',
+      justifyContent:'center',
+   },
+   row: {
+      flexDirection: 'row',
+      alignItems: 'baseline'
+   },
+   card: {
+      backgroundColor: 'white',
+      width: '90%',
+      // height: 100,
+      marginTop: 20,
+      paddingVertical: 20,
+      paddingHorizontal: 30,
+      justifyContent: 'center',
+      // alignItems: 'space-around',
+      // flexDirection: 'row',
+
+      // shadowColor: "#000",
+      // shadowOffset: {
+      //    width: 0,
+      //    height: 1,
+      // },
+      // shadowOpacity: 0.22,
+      // shadowRadius: 2.22,
+      // borderRadius: 2,
+      // elevation: 3,
+   },
+   label: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginBottom: 7,
+      marginRight: 5,
+   },
+   chartContainer: {
+      backgroundColor: 'white',
+      width: '90%',
+      marginTop: 20,
+      paddingVertical: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+   }
 });
  
