a7783d0b4a17c5a8a090e2e2dc28cc7a98ef2ce5
chalalala <neko_hime1212@yahoo.com>
2020-10-31 13:06:26 +0700

    First commit

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Constants from 'expo-constants';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const Dashboard = ({route}) => {
+  return(
+    <View style={styles.container}>
+      <Text>{route.name}</Text>
+    </View>
+  )
+}
+
+const Analytics = ({route}) => {
+  return(
+    <View style={styles.container}>
+      <Text>{route.name}</Text>
+    </View>
+  )
+}
+
+const Account = ({route}) => {
+  return(
+    <View style={styles.container}>
+      <Text>{route.name}</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused}) => {
+            if (route.name === "Dashboard"){
+              return <MaterialCommunityIcons name={"view-dashboard"} size={24} color={focused ? "blue" : "grey"} />;
+            }
+            else if (route.name === "Analytics"){
+              return <MaterialCommunityIcons name="google-analytics" size={24} color={focused ? "blue" : "grey"} />;
+            }
+            else if (route.name === "Account"){
+              return <MaterialCommunityIcons name="account" size={24} color={focused ? "blue" : "grey"} />;
+            }
+          }
+        })}
+  
+        tabBarOptions={{
+          activeTintColor: "blue",
+          inactiveTintColor: "grey"
+        }}
+      >
+        <Tab.Screen name="Dashboard" component={Dashboard}/>
+        <Tab.Screen name="Analytics" component={Analytics}/>
+        <Tab.Screen name="Account" component={Account}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
